90a3f50ca3f4ee497367d59d9988f2c33202b1d5
Ramon Abreu <cdevabreu@github.com>
2020-07-01 17:00:48 -0300

    01-Conceitos Iniciais

--- 01_iniciais.py
+++ 01_iniciais.py
@@ -18,25 +18,89 @@ o comentário também pode ser feito dessa forma
 se for um comentário grande ou que você precise
 fazer uma quebra de linhas.
 '''
+#isto é um comentátio"
+'''
+Conhecendo Alguns Tipos de
+Números com o Python :
+'''
 
-#print serve para imprimir um resultado na tela
+#Inteiros  = int(1,2,3,4,9,6,7,8,9)
+int (1)
+int('9')
+
+#Ponto Flutuante, decimais = float(2.2 , 1.0 , 9.5....)
+float(2.5)
+float(7.4)
+
+#complexos  = complex(7,2)
+complex(7,2)
+
+'''
+print serve para imprimir um resultado na tela
+Ele serve tanto para Strings(palavras/frases),
+Números(int,float,complex),Constantes e Variáveis.
+'''
+
+#Fazendo nossa primeira impressão : Lembrar de usar print seguido de ("")
+
 print ("hello word!")
+
+'''
+Agora Que já sabemos os Tipos de números e como obter resultado na tela,
+podemos realizar algumas operações utilizando os operadores numéricos.
+'''
 
 #Algumas operações:
 
 #Soma
 print(1+1) 
+
 #subtração
 print(3-2)
+
 #multiplicação
 print(2*2)
+
 #Divisão
 print(2/2)
+
 #Potenciação
 print(2**2)
+
 #Divisão parte inteira
 print(15//2)
+
 #Módulo/Resto
 print(15%2)
 
-import pacote.sub.doc1
+'''
+Também é possível Realizar operações com os operadores Lógicos
+Eles irão ajudar em algumas comparações.
+Os resultados para as operações serão True(verdadeiro) e False(Falso).
+'''
+#Verificar a Igualdade entre dois valores:
+print(2==2)
+
+#verificar a Diferença entre dois tipos :
+print(10 != 2)
+
+#Comparar se A é maior que B:
+print(3 > 10)
+
+#Comprar se A é menor que B:
+print(3 < 10)
+
+#Comparar se o A é maior ou igual que B:
+print(7 >= 10)
+
+#Comparar se o A é menor ou igual que B:
+print(7 <= 10)
+
+#Verificar de A está contido em um conjunto:
+print(5 in [15,10,43,2,1])
+
+
+
+
+
+
--- pacote/sub/doc1.py
+++
@@ -1,2 +0,0 @@
-
-print ("inportação concluída!")
